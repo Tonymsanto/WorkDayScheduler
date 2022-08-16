2f3f8fcaa6b13e8ded689b4f9075e7d0745850bb
Anthony Santo <anthonysanto@anthonys-mbp.lan>
2022-08-15 18:33:06 -0600

    Added real time clock and started on schedule grid

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var currentDay = moment().format("MMMM Do YYYY, h:mm:ss a");
+    var currentDayEl = $("#currentDay");
+    currentDayEl.text(currentDay);
+    console.log(currentDay);
+
+    $(".saveBtn").on("click", function () {
+        var text=
+    })
+})
