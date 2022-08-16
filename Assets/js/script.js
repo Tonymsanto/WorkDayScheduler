f6bd348e5e8432c5e1415b4137ccde3552ba6641
Anthony Santo <anthonysanto@anthonys-mbp.lan>
2022-08-15 21:22:39 -0600

    Added time slots to schedule. Working on saving events.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,43 @@ $(document).ready(function () {
     currentDayEl.text(currentDay);
     console.log(currentDay);
 
-    $(".saveBtn").on("click", function () {
-        var text=
+    var button = $(".saveBtn");
+    button.click(function () {
+        var usrText = $(".description").val();
+        var blockTime = $(".description").parent().attr("id");
+        console.log(usrText);
+        console.log(blockTime);
+
+        window.localStorage.setItem(blockTime, usrText);
     })
+
+    var hourEight = $("#hour8 .description");
+    hourEight.val(window.localStorage.getItem("hour8"));
+
+    var hourNine = $("#hour9 .description");
+    hourNine.val(window.localStorage.getItem("hour9"));
+
+    var hourTen = $("#hour10 .description");
+    hourTen.val(window.localStorage.getItem("hour10"));
+
+    var hourEleven = $("#hour11 .description");
+    hourEleven.val(window.localStorage.getItem("hour11"));
+
+    var hourTwelve = $("#hour12 .description");
+    hourTwelve.val(window.localStorage.getItem("hour12"));
+
+    var hourThirteen = $("#hour13 .description");
+    hourThirteen.val(window.localStorage.getItem("hour13"));
+
+    var hourFourteen = $("#hour14 .description");
+    hourFourteen.val(window.localStorage.getItem("hour14"));
+
+    var hourFifteen = $("#hour15 .description");
+    hourFifteen.val(window.localStorage.getItem("hour15"));
+
+    var hourSixteen = $("#hour16 .description");
+    hourSixteen.val(window.localStorage.getItem("hour16"));
+
+    var hourSeventeen = $("#hour17 .description");
+    hourSeventeen.val(window.localStorage.getItem("hour17"));
 })
